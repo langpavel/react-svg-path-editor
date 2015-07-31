bb3fb2469fcefbc854cafbe2a4327dd9e8300686
Pavel Lang <langpavel@phpskelet.org>
2015-07-31 16:43:06 +0200

    loading background image

--- src/client/lib/svg-editor/svgEditor.react.js
+++ src/client/lib/svg-editor/svgEditor.react.js
@@ -11,6 +11,7 @@ export default class SvgEditor extends React.Component {
       viewBoxMinY: -20,
       viewBoxWidth: 40,
       viewBoxHeight: 40,
+      zoom: 5,
     };
   }
 
@@ -24,17 +25,38 @@ export default class SvgEditor extends React.Component {
   }
 
   render() {
-    const {path} = this.state || {};
+    const {path, backgroundImage} = this.state || {};
     const viewBox = this.getViewBox();
     const svgStyle = {
       strokeWidth: 1,
       stroke: 'rgba(0,0,0,0.7)',
       fill: 'rgba(0,0,0,0.1)',
-      fillRule: 'evenodd'
+      fillRule: 'evenodd',
     };
 
     return (
       <div className="svg-editor">
+        <div className="name-value">
+          <label>
+            <span>zoom:</span>
+            <input
+              type="number"
+              defaultValue={this.state.zoom}
+              onChange={({target: {value}}) => {
+                const val = parseFloat(value);
+                if (isNaN(val)) return;
+                this.setState({zoom: val})
+              }}
+            />
+          </label>
+        </div>
+
+        <div className="background-file">
+          <label>
+            <span>background:</span>
+            <input type="file" accept="image/*" onChange={(e) => this.uploadFile(e)} />
+          </label>
+        </div>
         <div className="viewBox-editor">
           {`viewBox="${this.getViewBox()}" `}
           <div className="name-value">
@@ -101,11 +123,52 @@ export default class SvgEditor extends React.Component {
           path={path}
           onChange={({target: {value}}) => this.setSvgPath(value)}
         />
-        <svg width='800' height='800' viewBox={viewBox} style={svgStyle}>
-          <path d={path} />
-        </svg>
+        <div className="work-area">
+          {backgroundImage && <img
+            ref="background"
+            className="background"
+            src={backgroundImage}
+            width={this.state.viewBoxWidth * this.state.zoom}
+            height={this.state.viewBoxHeight * this.state.zoom}
+          />}
+          <svg
+            className="surface"
+            width={this.state.viewBoxWidth * this.state.zoom}
+            height={this.state.viewBoxHeight * this.state.zoom}
+            viewBox={viewBox}
+            style={svgStyle}
+          >
+            <path d={path} />
+          </svg>
+        </div>
       </div>
     );
+  }
+
+  uploadFile(e) {
+    const {target} = e;
+    const file = target.files && target.files[0];
+
+    if (file && FileReader) {
+        var fr = new FileReader();
+        fr.onload = () => {
+            console.log('file', fr.result);
+            this.setState({
+              backgroundImage: fr.result
+            }, () => this.onBackgroundImageSet());
+        }
+        fr.readAsDataURL(file);
+    }
+
+  }
+
+  onBackgroundImageSet() {
+    const img = React.findDOMNode(this.refs.background);
+    const {naturalWidth, naturalHeight} = img;
+    this.setState({
+      viewBoxWidth: naturalWidth,
+      viewBoxHeight: naturalHeight,
+    });
   }
 
 }
